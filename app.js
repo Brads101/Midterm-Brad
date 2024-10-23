99e738b5dbaf9bd65168d0bc3a39b7117f276b51
Brads101 <bradford.samson05@gmail.com>
2024-10-23 14:04:06 +0800

    app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+function sendMail() {
+    event.preventDefault(); // Prevent default form submission
+
+    document.getElementById('loading').style.display = 'flex';
+
+    const fullName = document.getElementById('fullName').value;
+    const email = document.getElementById('email_id').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+
+    // Here you would use emailjs or another service to send the email
+    emailjs.send("service_28nhc7s", "template_rennl1d9d", {
+        from_name: fullName,
+        from_email: email,
+        subject: subject,
+        message: message,
+    })
+    .then(() => {
+        document.getElementById('loading').style.display = 'none';
+        alert('Email sent successfully!');
+    })
+
+    
+    .catch((error) => {
+        document.getElementById('loading').style.display = 'none';
+        console.error('Error:', error);
+        alert('Error sending email. Please try again later.');
+    });
+}
